2924baf32a8107be4a754d54a1179a8dc197a946
Jim Carr <jfcarr@gmail.com>
2019-08-20 15:57:15 -0400

    Documentation comments in the doe header.

--- doe/doe.h
+++ doe/doe.h
@@ -1,13 +1,16 @@
 #ifndef DOE_H
 #define DOE_H
 
-class CDoe {
+/** This class provides functionality for determining the date of Easter. */
+class CDoe
+{
 private:
 	float year;
 
 public:
 	CDoe(float year);
 
+	/** Return a string representing the date of Easter, for the year specified at init.  */
 	std::string GetEaster();
 };
 #endif

ca98e79926af224b82ccacd85d0a4523a8c85e90
Jim Carr <jfcarr@gmail.com>
2019-08-22 19:59:44 -0400

    Cleanup and additional comments.

--- julian/client-py.py
+++ julian/client-py.py
@@ -6,7 +6,6 @@ julianlib = ctypes.cdll.LoadLibrary('./julianshared.so')
 
 class Julian(object):
 	def __init__(self):
-		#julianlib.CJulian_new.argtypes = [ctypes.c_int, ctypes.c_float, ctypes.c_int]
 		julianlib.CJulian_new.restype = ctypes.c_void_p
 
 		julianlib.CJulian_GreenwichToJulian.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_float, ctypes.c_int]
--- julian/julian.h
+++ julian/julian.h
@@ -1,20 +1,22 @@
 #ifndef JULIAN_H
 #define JULIAN_H
 
+/** Simple Greenwich Date structure (month, fractional day, year) */
 struct GreenwichDate {
 	int month;
 	float day;
 	int year;
 };
 
-/** This class provides functionality for determining the Julian date. */
+/** This class provides functionality for working with Julian and Greenwich dates. */
 class CJulian {
 public:
 	CJulian();
 
-	/** Return a float representing the Julian date, for the Greenwich month, day, and year specified.  */
+	/** Return a float representing the Julian date for the Greenwich month, day, and year specified.  */
 	float GreenwichToJulian(int month, float day, int year);
 
+	/** Return a GreenwichDate object for the Julian Date specified.  */
 	GreenwichDate JulianToGreenwich(float julianDate);
 };
 #endif
